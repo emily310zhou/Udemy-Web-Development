3270075a4d9ee433e3559d469f3c4243da372069
emily310zhou <66382705+emily310zhou@users.noreply.github.com>
2021-08-22 22:19:57 -0500

    Add files via upload

---
+++ Back End/intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+var myVillainName = supervillains.random();
+console.log(myVillainName);
